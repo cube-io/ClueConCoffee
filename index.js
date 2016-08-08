42ec03887b61c1030d34e375ad83f19577ece444
Asbjørn Thegler <asbjoern@gmail.com>
2016-08-08 15:37:16 -0500

    Index now uses diodes

--- index.js
+++ index.js
@@ -1,2 +1,49 @@
+var gpio = require("gpio");
+var mqtt    = require("mqtt");
+var mqttClient  = mqtt.connect("mqtt://test.mosquitto.org");
+var FlowrouteSMS = require("flowroute-sms");
+var smsClient = new FlowrouteSMS("06394246", "kVGxBeat0WMoCazTU9BvQC0vjjeSedxe");
 
-console.log("Not implemented");
+var seatNumber = "F8";
+var from = "12015354459";
+// var to = "12015354459";
+var to   = "17738015085";
+var green = gpio.export(18);
+var red = gpio.export(19);
+
+
+mqttClient.on("connect", function() {
+    mqttClient.subscribe("fromFlowRouteCoffee");
+});
+
+mqttClient.on("message", function(topic, message) {
+    var data = JSON.parse(message.toString());
+
+    console.log(data.body)
+    if(data.body === "What is your seat number?") {
+        console.log(seatNumber);
+        smsClient.send(to, from, seatNumber)
+        .then(function(result) {
+            console.log("Sent message from " + from + " to " + to + " with body: " + seatNumber, result.id);
+            return;
+        });
+    }
+
+    if(data.body === "Your order has been received!") {
+        green.set(1);
+        setTimeout(() => {green.set(0); }, 1000);
+        return;
+    }
+
+    red.set(1);
+    setTimeout(() => {red.set(0); }, 1000);
+});
+
+var button = gpio.export(17, {
+    direction: "in",
+});
+
+smsClient.send(to, from, "NITRO")
+.then(function(result) {
+    console.log("Sent message from " + from + " to " + to + " with body: NITRO", result.id);
+});
